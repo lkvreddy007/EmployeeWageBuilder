7efc142c506490c50809eaeb592e278a1a65c8cf
lkvreddy007 <lkvreddy007@gmail.com>
2020-09-25 18:44:57 +0530

    [Krishna]added-CalculatingEmpWage

--- src/EmpWageBuilder.java
+++ src/EmpWageBuilder.java
@@ -2,5 +2,20 @@
 public class EmpWageBuilder {
 	public static void main(String[] args) {
 		System.out.println("Employee Wage Builder");
+		int IS_FULL_TIME=1;
+		int EMP_RATE_PER_HOUR=20;
+		
+		int empHrs=0;
+		int empWage=0;
+		
+		double empCheck=Math.floor(Math.random()*10)%2;
+		if(empCheck== IS_FULL_TIME) {
+			empHrs=8;
+		}
+		else {
+			empHrs=0;
+		}
+		empWage= empHrs*EMP_RATE_PER_HOUR;
+		System.out.println("Employee Wage: "+empWage);
 	}
 }
