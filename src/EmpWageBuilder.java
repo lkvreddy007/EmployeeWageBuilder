55d127859685f3fe4a4471481d4457350c6d0af2
lkvreddy007 <lkvreddy007@gmail.com>
2020-09-25 18:35:50 +0530

    [Krishna]added-EmployeeAttendance

--- src/EmpWageBuilder.java
+++ src/EmpWageBuilder.java
@@ -1,6 +1,13 @@
 
 public class EmpWageBuilder {
 	public static void main(String[] args) {
-		System.out.println("Employee Wage Builder");
+		int IS_FULL_TIME=1;
+		double empcheck= Math.floor(Math.random()*10)%2; 
+		if(empcheck==IS_FULL_TIME) {
+			System.out.println("Employee present");
+		}
+		else {
+			System.out.println("Employee absent");
+		}
 	}
 }
