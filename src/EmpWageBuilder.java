d07cb85a106987fed8d7a67636479e139c8fcb6b
lkvreddy007 <44741736+lkvreddy007@users.noreply.github.com>
2020-09-25 18:51:26 +0530

    Update EmpWageBuilder.java

--- src/EmpWageBuilder.java
+++ src/EmpWageBuilder.java
@@ -6,9 +6,9 @@ public class EmpWageBuilder {
 		
 
 		int IS_FULL_TIME=1;
-    int EMP_RATE_PER_HOUR=20;
+    		int EMP_RATE_PER_HOUR=20;
     
-    int empHrs=0;
+    		int empHrs=0;
 		int empWage=0;
     
 		double empcheck= Math.floor(Math.random()*10)%2; 
