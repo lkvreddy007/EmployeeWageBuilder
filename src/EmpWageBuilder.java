fa91960626064f739965e4541d1c804a45f32526
lkvreddy007 <44741736+lkvreddy007@users.noreply.github.com>
2020-09-25 20:11:53 +0530

    Update EmpWageBuilder.java

--- src/EmpWageBuilder.java
+++ src/EmpWageBuilder.java
@@ -8,34 +8,34 @@ public class EmpWageBuilder {
 	
 	public static int computeEmpWage(){
 		int empHrs=0;
-    	int empWage=0;
-        int totalEmpWage=0;
-        int totalEmpHrs=0;
-        int totalWorkingDays=0;
-        while(totalEmpHrs<=MAX_HRS_IN_MONTH && totalWorkingDays<NUM_OF_WORKING_DAYS) {
-        	totalWorkingDays+=1;
-        	int empCheck=(int) Math.floor(Math.random()*10) % 3;
-        	switch(empCheck) {
-				case IS_PART_TIME:
-					empHrs=4;
-					System.out.println("Employee is Part time employee");
-					break;
-				case IS_FULL_TIME:
-					empHrs=8;
-					System.out.println("Employee is Full time employee");
-					break;
-				default:
-					empHrs=0;	
-			}
-        	totalEmpHrs+=empHrs;
-			System.out.println("Day#: "+totalWorkingDays +" Emp Hr: "+empHrs);
-        }
-        totalEmpWage=totalEmpHrs*EMP_RATE_PER_HOUR;
-        System.out.println("Total Emp Wage: "+totalEmpWage);
-        return totalEmpWage;
+    		int empWage=0;
+        	int totalEmpWage=0;
+       		int totalEmpHrs=0;
+       	 	int totalWorkingDays=0;
+		while(totalEmpHrs<=MAX_HRS_IN_MONTH && totalWorkingDays<NUM_OF_WORKING_DAYS) {
+			totalWorkingDays+=1;
+			int empCheck=(int) Math.floor(Math.random()*10) % 3;
+			switch(empCheck) {
+					case IS_PART_TIME:
+						empHrs=4;
+						System.out.println("Employee is Part time employee");
+						break;
+					case IS_FULL_TIME:
+						empHrs=8;
+						System.out.println("Employee is Full time employee");
+						break;
+					default:
+						empHrs=0;	
+				}
+			totalEmpHrs+=empHrs;
+				System.out.println("Day#: "+totalWorkingDays +" Emp Hr: "+empHrs);
+        	}
+        	totalEmpWage=totalEmpHrs*EMP_RATE_PER_HOUR;
+        	System.out.println("Total Emp Wage: "+totalEmpWage);
+        	return totalEmpWage;
 	}
 	public static void main(String[] args) {
 		System.out.println("Employee Wage Builder");
-    	computeEmpWage();
+    		computeEmpWage();
 	}
 }
