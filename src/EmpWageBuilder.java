f8682af90d679e4631cbd1d9a2b97f90f4513d6d
lkvreddy007 <lkvreddy007@gmail.com>
2020-09-25 19:00:45 +0530

    [Krishna]added-ParttimeEmployee

--- src/EmpWageBuilder.java
+++ src/EmpWageBuilder.java
@@ -4,24 +4,19 @@ public class EmpWageBuilder {
 
 		System.out.println("Employee Wage Builder");
 		
-
-		int IS_FULL_TIME=1;
-    		int EMP_RATE_PER_HOUR=20;
+		int IS_PART_TIME=1;
+		int IS_FULL_TIME=2;
+    	int EMP_RATE_PER_HOUR=20;
     
-    		int empHrs=0;
+    	int empHrs=0;
 		int empWage=0;
-    
-		double empcheck= Math.floor(Math.random()*10)%2; 
-		if(empcheck==IS_FULL_TIME) {
-			System.out.println("Employee present");
-		}
-		else {
-			System.out.println("Employee absent");
-		}
-    
-		double empCheck=Math.floor(Math.random()*10)%2;
+        
+		double empCheck=Math.floor(Math.random()*10)%3;
 		if(empCheck== IS_FULL_TIME) {
 			empHrs=8;
+		}
+		else if(empCheck== IS_PART_TIME) {
+			empHrs=4;
 		}
 		else {
 			empHrs=0;
